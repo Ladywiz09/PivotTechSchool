3ab31a8eeb8e3656dc23157f6598d28a827ec8d6
Ladywiz09 <alisha.croom@shipt.com>
2022-11-29 19:10:15 -0600

    work in progress

--- productserver/main.go
+++ productserver/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Gorilla!\n"))
+type product struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
 }
 
-func main() {
-	r := mux.NewRouter()
-	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+var products []product
 
-	// Bind to a port and pass our router in
-	log.Fatal(http.ListenAndServe("localhost:8000", r))
+func getYourHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+	if err := json.NewEncoder(w).Encode(products); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+}
+func initProducts() {
+	bs, err := os.ReadFile("products.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal(bs, &products); err != nil {
+		log.Fatal(err)
+	}
+}
+func main() {
+	initProducts()
+	r := mux.NewRouter()
+	r.HandleFunc("/products", getYourHandler)
+
+	log.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
