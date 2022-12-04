b6978044f01dd4737ffc61e294f6ce489dc2beff
Ladywiz09 <alisha.croom@shipt.com>
2022-12-04 02:38:52 -0600

    updated part 1

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Ladywiz09/pivottechschool
 go 1.19
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/mattn/go-sqlite3 v1.14.16 // indirect
--- seeder/main.go
+++ seeder/main.go
@@ -2,109 +2,153 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
+	"flag"
 	"log"
+	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	os.Remove("./foo.db")
+type product struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Price       int    `json:"price"`
+	Description string `json:"description"`
+}
 
-	db, err := sql.Open("sqlite3", "./foo.db")
+var products []product
+
+func getProductsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+
+	p := productlookup(id)
+	if p == nil {
+		log.Printf("Product with id %d not found", id)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(p); err != nil {
+		log.Println("Error encoding product to json", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+}
+
+func productlookup(id int) *product {
+	for _, p := range products {
+		if p.Id == id {
+			return &p
+		}
+	}
+	return nil
+}
+
+func deleteProductsHandler(w http.ResponseWriter, r *http.Request) {
+	os.Remove("products.db")
+	w.WriteHeader(http.StatusOK)
+}
+
+func addProductHandler(w http.ResponseWriter, r *http.Request) {
+	var p product
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+	products = append(products, p)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func readProducts() {
+	bs, err := os.ReadFile("products.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(bs, &products)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() error {
+	var productsDB string
+	var jsonProducts string
+
+	flag.StringVar(&productsDB, "db", "products.db", "Database file")
+	flag.StringVar(&jsonProducts, "json", "products.json", "JSON file")
+	flag.Parse()
+
+	db, err := sql.Open("sqlite3", productsDB)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	sqlStmt := `
-	create table foo (id integer not null primary key, name text);
-	delete from foo;
-	`
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
+	prod, err := os.ReadFile(jsonProducts)
+	if err := db; err != nil {
+		log.Fatal(err)
 	}
 
+	var payload product
+
+	if err := json.Unmarshal(prod, &payload); err != nil {
+		return err
+		}
+	return nil
+}
 	tx, err := db.Begin()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	stmt, err := tx.Prepare("INSERT INTO products(id, name, price, description) VALUES(?, ?, ?, ?)")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	defer stmt.Close()
-	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(i, fmt.Sprintf("こんにちは世界%03d", i))
+
+	for _, p := range prod {
+		_, err = stmt.Exec(p.Id, p.Name, p.Price, p.Description)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 	}
+
 	err = tx.Commit()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("select id, name from foo")
+	return nil
+
+	rows, err := db.Query("SELECT id, name, price, description FROM products")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
+
 	for rows.Next() {
 		var id int
 		var name string
-		err = rows.Scan(&id, &name)
+		var price int
+		var description string
+		err = rows.Scan(&id, &name, &price, &description)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(id, name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	stmt, err = db.Prepare("select name from foo where id = ?")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	var name string
-	err = stmt.QueryRow("3").Scan(&name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(name)
+		fmt.Println(id, name, price, description)
 
-	_, err = db.Exec("delete from foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err = db.Query("select id, name from foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
+		err = rows.Err()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(id, name)
 	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+
